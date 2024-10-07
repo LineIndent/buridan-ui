8107a30bfa5b65d45f1eeb2466c4351fb5e66253
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-07 19:17:50 +0300

    Additional routes added

---
+++ buridan_ui/routes/interactive_tables.py
@@ -0,0 +1,3 @@
+INTERACTIVE_TABLES: list[dict[str, str]] = [
+    {"name": "Dashboard", "path": "/interactive-table/dashboard", "is_beta": True}
+]
--- buridan_ui/routes/started_routes.py
+++ buridan_ui/routes/started_routes.py
@@ -1,6 +1,7 @@
 GETTING_STARTED_ROUTES = [
+    {"name": "Introduction", "path": "/getting-started/introduction"},
     {"name": "Installation", "path": "/getting-started/installation"},
     {"name": "Who is Buridan?", "path": "/getting-started/who-is-buridan"},
-    {"name": "FAQs", "path": "/getting-started/faqs"},
-    {"name": "License", "path": "/getting-started/license"},
+    # {"name": "FAQs", "path": "/getting-started/faqs"},
+    # {"name": "License", "path": "/getting-started/license"},
 ]
