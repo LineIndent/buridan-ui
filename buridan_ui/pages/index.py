4fe16f03f42ba766c6b210903a2f6db52dbbd96c
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-10 21:39:48 +0300

    Updated index page

--- buridan_ui/pages/index.py
+++ buridan_ui/pages/index.py
@@ -3,8 +3,10 @@ import asyncio
 import reflex as rx
 from reflex import color
 
-from ..templates.shared.navbar import right_items, left_items
 from .thumbnail_items.exports import export_thumbnail
+
+from ..templates.shared.navbar import right_items, left_items
+from ..routes.pantry_routes import PANTRY_ROUTES
 
 
 def wrapper(title: str, instructions: str, tag: str):
@@ -49,16 +51,7 @@ def wrapper(title: str, instructions: str, tag: str):
     )
 
 
-DOTS: dict = {
-    "@keyframes dots": {
-        "0%": {"background_position": "0 0"},
-        "100%": {"background_position": "0px 100%"},
-    },
-    "animation": "dots 90s linear infinite",
-}
-
-
-def create_background(pos_x: str, pos_y: str):
+def create_background():
     return rx.box(
         background_size="90px 90px",
         background_image=(
@@ -66,11 +59,10 @@ def create_background(pos_x: str, pos_y: str):
             "linear-gradient(to right, transparent 99%, hsl(0, 0%, 39%) 100%)"
         ),
         mask=(
-            "radial-gradient(25% 25% at 75% 75%, hsl(0, 0%, 0%, 0.75), hsl(0, 0%, 0%, 0)), "  # Original mask
+            # "radial-gradient(25% 25% at 75% 75%, hsl(0, 0%, 0%, 0.75), hsl(0, 0%, 0%, 0)), "
             "radial-gradient(45% 45% at 50% 50%, hsl(0, 0%, 0%, 0.60), hsl(0, 0%, 0%, 0)), "
-            "radial-gradient(30% 50% at 50% 50%, hsl(0, 0%, 0%, 0.5), hsl(0, 0%, 0%, 0)), "  # Inner gradient for depth
+            # "radial-gradient(30% 50% at 50% 50%, hsl(0, 0%, 0%, 0.5), hsl(0, 0%, 0%, 0)), "
             "radial-gradient(60% 70% at 50% 50%, hsl(0, 0%, 0%, 0.35), hsl(0, 0%, 0%, 0))"
-            # Outer gradient for a subtle fade
         ),
         width="100%",
         height="100%",
@@ -87,7 +79,7 @@ def create_navigation():
                     src="/logo.jpg",
                     width="28px",
                     height="28px",
-                    border_radius="15%",
+                    border_radius="49%",
                     object_fit="fit",
                     border=f"1px solid {rx.color('slate', 12)}",
                 ),
@@ -95,36 +87,105 @@ def create_navigation():
                 align="center",
                 padding="0px 14px",
             ),
-            right_items(),
+            rx.box(
+                right_items(),
+                padding="0px 14px",
+            ),
             align="center",
             justify="between",
             padding="18px 0px",
             backdrop_filter="blur(4px)",
             max_width="75em",
             width="100%",
+            position="fixed",
         ),
         width="100%",
-        z_index="20",
-        max_width="75em",
-        position="relative",  # Change to relative
+        z_index="100",
+        max_width="70em",
+        position="relative",
         justify_content="center",
-        padding="12px 0",  # Optional: add some padding for breathing space
+        top="0",
     )
 
 
-class Animate(rx.State):
-    can_show: str = "0"
-    width: str = "0px"
+ROOT = dict(
+    width="100%",
+    align="center",
+    justify="start",
+    overflow="scroll",
+    padding="0px 12px",
+    min_height="100vh",
+    background=rx.color("gray", 3),
+)
 
-    icon: str = (
-        "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNsaXBib2FyZC1saXN0Ij48cmVjdCB3aWR0aD0iOCIgaGVpZ2h0PSI0IiB4PSI4IiB5PSIyIiByeD0iMSIgcnk9IjEiLz48cGF0aCBkPSJNMTYgNGgyYTIgMiAwIDAgMSAyIDJ2MTRhMiAyIDAgMCAxLTIgMkg2YTIgMiAwIDAgMS0yLTJWNmEyIDIgMCAwIDEgMi0yaDIiLz48cGF0aCBkPSJNMTIgMTFoNCIvPjxwYXRoIGQ9Ik0xMiAxNmg0Ii8+PHBhdGggZD0iTTggMTFoLjAxIi8+PHBhdGggZD0iTTggMTZoLjAxIi8+PC9zdmc+"
+CONTENT = dict(
+    spacing="1",
+    z_index="50",
+    width="100%",
+    padding="12px",
+    align="center",
+    justify="start",
+    max_width="70em",
+    text_align="center",
+)
+
+
+def create_section_header(title: str, description: str):
+    return rx.vstack(
+        rx.heading(
+            title,
+            weight="bold",
+            size="8",
+            font_family="var(--chakra-fonts-serif)",
+            color=rx.color("slate", 12),
+        ),
+        rx.text(
+            description,
+            size="4",
+            weight="medium",
+            max_width="45em",
+            color=rx.color("slate", 11),
+        ),
+        width="100%",
+        text_align="start",
+        padding="14px 0px",
     )
 
-    async def toggle_preview(self):
-        self.icon = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNoZWNrIj48cGF0aCBkPSJNMjAgNiA5IDE3bC01LTUiLz48L3N2Zz4="
+
+def create_footer_item(title: str, item_list: list[dict[str, str]]):
+    return rx.vstack(
+        rx.text(title, weight="bold", size="2", color=rx.color("slate", 11)),
+        rx.hstack(
+            *[
+                rx.text(
+                    item["name"],
+                    weight="bold",
+                    size="1",
+                    text_align="start",
+                    color=rx.color("slate", 12),
+                )
+                for item in item_list
+            ],
+            display="grid",
+            grid_template_columns=[
+                f"repeat({i}, minmax(0, 1fr))" for i in [2, 2, 3, 4, 4]
+            ],
+            justify="start",
+            width="100%",
+            gap="1rem 3rem",
+        ),
+        width="100%",
+    )
+
+
+class Index(rx.State):
+    default_icon: bool = True
+
+    async def toggle_icon(self):
+        self.default_icon = False
         yield
         await asyncio.sleep(1)
-        self.icon = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNsaXBib2FyZC1saXN0Ij48cmVjdCB3aWR0aD0iOCIgaGVpZ2h0PSI0IiB4PSI4IiB5PSIyIiByeD0iMSIgcnk9IjEiLz48cGF0aCBkPSJNMTYgNGgyYTIgMiAwIDAgMSAyIDJ2MTRhMiAyIDAgMCAxLTIgMkg2YTIgMiAwIDAgMS0yLTJWNmEyIDIgMCAwIDEgMi0yaDIiLz48cGF0aCBkPSJNMTIgMTFoNCIvPjxwYXRoIGQ9Ik0xMiAxNmg0Ii8+PHBhdGggZD0iTTggMTFoLjAxIi8+PHBhdGggZD0iTTggMTZoLjAxIi8+PC9zdmc+"
+        self.default_icon = True
 
 
 @rx.page("/", "buridan-ui")
@@ -132,17 +193,17 @@ def index():
     return rx.vstack(
         create_navigation(),
         rx.vstack(
-            rx.divider(height="4em", opacity="0"),
+            rx.divider(height="10em", opacity="0"),
             rx.hstack(
-                rx.heading(
-                    "Buridan UI",
-                    weight="bold",
-                    size="2",
-                    letter_spacing="-1px",
-                    font_family="var(--chakra-fonts-serif)",
-                    color=rx.color("slate", 11),
-                ),
-                rx.separator(orientation="vertical", width="1.25px", height="30px"),
+                # rx.heading(
+                #     "Buridan UI",
+                #     weight="bold",
+                #     size="2",
+                #     letter_spacing="-1px",
+                #     font_family="var(--chakra-fonts-serif)",
+                #     color=rx.color("slate", 11),
+                # ),
+                # rx.separator(orientation="vertical", width="1.25px", height="30px"),
                 *[
                     rx.badge(
                         rx.heading(
@@ -181,9 +242,9 @@ def index():
                 rx.button(
                     "Getting Started",
                     variant="surface",
-                    color_scheme="gray",
                     flex="4",
                     size="3",
+                    color_scheme="gray",
                     z_index="20",
                 ),
                 rx.button(
@@ -200,24 +261,9 @@ def index():
                 padding="24px 0px",
             ),
             rx.divider(height="12em", opacity="0"),
-            rx.vstack(
-                rx.heading(
-                    "Components Entirely Built With Reflex",
-                    weight="bold",
-                    size="8",
-                    font_family="var(--chakra-fonts-serif)",
-                    color=rx.color("slate", 12),
-                ),
-                rx.text(
-                    "A full-stack framework complete with built-in features, including a comprehensive theming system, ready-to-use UI components, and customizable elements.",
-                    size="4",
-                    weight="medium",
-                    color=rx.color("slate", 11),
-                    max_width="60em",
-                ),
-                width="100%",
-                text_align="start",
-                padding="14px 0px",
+            create_section_header(
+                "Components Entirely Built With Reflex",
+                "A full-stack framework complete with built-in features, including a comprehensive theming system, ready-to-use UI components, and customizable elements.",
             ),
             rx.hstack(
                 wrapper(
@@ -235,37 +281,18 @@ def index():
                     "Our components are available under an open source license, empowering you to use, modify, and share them freely.",
                     "code",
                 ),
+                gap="2rem",
                 width="100%",
                 display="grid",
-                gap="2rem",
                 grid_template_columns=[
-                    "repeat(1, minmax(0, 1fr))",
-                    "repeat(1, minmax(0, 1fr))",
-                    "repeat(2, minmax(0, 1fr))",
-                    "repeat(3, minmax(0, 1fr))",
-                    "repeat(3, minmax(0, 1fr))",
+                    f"repeat({i}, minmax(0, 1fr))" for i in [1, 1, 2, 3, 3]
                 ],
                 padding="18px 0px",
             ),
             rx.divider(height="10em", opacity="0"),
-            rx.vstack(
-                rx.heading(
-                    "Reflex Components",
-                    weight="bold",
-                    size="8",
-                    font_family="var(--chakra-fonts-serif)",
-                    color=rx.color("slate", 12),
-                ),
-                rx.text(
-                    "Built on the Reflex framework, our components provide a set of prebuilt UI elements that streamline the development of responsive, web-based applications. Easily customize and adapt them to fit your unique design needs.",
-                    size="4",
-                    weight="medium",
-                    color=rx.color("slate", 11),
-                    max_width="60em",
-                ),
-                width="100%",
-                text_align="start",
-                padding="14px 0px",
+            create_section_header(
+                "Reflex Components",
+                "Built on the Reflex framework, our components provide a set of prebuilt UI elements that streamline the development of responsive, web-based applications.",
             ),
             rx.hstack(
                 *export_thumbnail,
@@ -273,34 +300,15 @@ def index():
                 display="grid",
                 gap="1rem",
                 grid_template_columns=[
-                    "repeat(2, minmax(0, 1fr))",
-                    "repeat(2, minmax(0, 1fr))",
-                    "repeat(3, minmax(0, 1fr))",
-                    "repeat(4, minmax(0, 1fr))",
-                    "repeat(4, minmax(0, 1fr))",
+                    f"repeat({i}, minmax(0, 1fr))" for i in [1, 2, 3, 4, 4]
                 ],
                 min_height="100vh",
                 padding="18px 0px",
             ),
             rx.divider(height="10em", opacity="0"),
-            rx.vstack(
-                rx.heading(
-                    "You're One Step Away From Shipping Your Web Application",
-                    weight="bold",
-                    size="8",
-                    font_family="var(--chakra-fonts-serif)",
-                    color=rx.color("slate", 12),
-                ),
-                rx.text(
-                    "Download and install Reflex to start building out your idea, or check our the get started pages for more information.",
-                    size="4",
-                    weight="medium",
-                    color=rx.color("slate", 11),
-                    max_width="60em",
-                ),
-                width="100%",
-                text_align="start",
-                padding="14px 0px",
+            create_section_header(
+                "You're One Step Away From Shipping Your Web Application",
+                "Download and install Reflex to start building out your idea, or check our the get started pages for more information.",
             ),
             rx.box(
                 rx.hstack(
@@ -308,30 +316,26 @@ def index():
                         "Get Started",
                         variant="surface",
                         color_scheme="gray",
-                        flex="3",
-                        size="4",
+                        flex="2",
+                        size="3",
                         z_index="20",
                     ),
                     rx.button(
                         "pip install reflex",
-                        rx.separator(
-                            orientation="vertical", width="1.25px", height="25px"
-                        ),
-                        rx.image(
-                            src=Animate.icon,
-                            size=14,
-                            color=rx.color("slate", 12),
-                            filter="invert(0.81)",
+                        rx.cond(
+                            Index.default_icon,
+                            rx.icon(tag="clipboard-list", size=14),
+                            rx.icon(tag="check", size=14, color=rx.color("grass", 12)),
                         ),
                         variant="soft",
                         color_scheme="gray",
-                        flex="5",
-                        size="4",
+                        flex="3",
+                        size="3",
                         z_index="20",
                         display="flex",
                         justify_content="space-between",
                         cursor="pointer",
-                        on_click=Animate.toggle_preview,
+                        on_click=Index.toggle_icon,
                     ),
                     max_width="25em",
                     justify="start",
@@ -339,28 +343,92 @@ def index():
                 ),
                 width="100%",
             ),
-            rx.divider(height="10em", opacity="0"),
+            rx.divider(height="2.5em", opacity="0"),
             rx.vstack(
-                rx.text(),
+                rx.spacer(),
+                rx.divider(height="15em", opacity="0"),
+                rx.hstack(
+                    rx.vstack(
+                        left_items(),
+                        rx.hstack(
+                            rx.link(
+                                rx.icon(
+                                    tag="github",
+                                    size=18,
+                                    color=rx.color("slate", 11),
+                                ),
+                                href="#",
+                                color_scheme="gray",
+                                bg=rx.color("gray", 4),
+                                border_radius="20%",
+                                padding="3.5px",
+                            ),
+                            rx.link(
+                                rx.icon(
+                                    tag="youtube",
+                                    size=18,
+                                    color=rx.color("slate", 11),
+                                ),
+                                href="#",
+                                color_scheme="gray",
+                                bg=rx.color("gray", 4),
+                                border_radius="20%",
+                                padding="3.5px",
+                            ),
+                            align="center",
+                        ),
+                        justify="center",
+                        flex=["100%", "100%", "100%", "20%", "20%"],
+                        align="start",
+                    ),
+                    rx.vstack(
+                        create_footer_item(
+                            "Home",
+                            [
+                                {"name": "Installation"},
+                                {"name": "Who is Buridan?"},
+                                {"name": "Interactive Tables"},
+                            ],
+                        ),
+                        rx.divider(height="1em", opacity="0"),
+                        create_footer_item(
+                            "Pantry",
+                            PANTRY_ROUTES,
+                        ),
+                        rx.divider(height="1em", opacity="0"),
+                        create_footer_item(
+                            "Resources",
+                            [
+                                {"name": "Reflex Framework"},
+                                {"name": "Source Code"},
+                                {"name": "GitHub"},
+                                {"name": "@LineIndent"},
+                            ],
+                        ),
+                        rx.divider(height="1em", opacity="0"),
+                        width="100%",
+                        flex=["100%", "100%", "100%", "50%", "65%"],
+                    ),
+                    width="100%",
+                    align="start",
+                    flex_wrap=[
+                        "wrap-reverse",
+                        "wrap-reverse",
+                        "wrap-reverse",
+                        "wrap",
+                        "wrap",
+                    ],
+                ),
                 width="100%",
-                height="30em",
-                bg=rx.color("gray", 1, True),
+                border_top=f"1px solid {rx.color('gray', 12)}",
+                padding="32px 0px",
+                bg=rx.color("gray", 3),
+                mask="linear-gradient(to top, hsl(0, 0%, 0%, 1) 50%, hsl(0, 0%, 0%, 0))",
+                align="end",
+                justify="between",
             ),
-            width="100%",
-            padding="12px",
-            spacing="1",
-            align="center",
-            justify="start",
-            text_align="center",
-            z_index="50",
-            max_width="75em",
+            **CONTENT,
         ),
-        create_background("100%", "100%"),
-        width="100%",
-        min_height="100vh",  # Change this to allow it to grow
-        overflow="scroll",  # Keep this to allow for internal scrolling
-        align="center",
-        justify="start",
-        padding="0px 12px",
-        background=rx.color("gray", 3),
+        create_background(),
+        **ROOT,
     )
