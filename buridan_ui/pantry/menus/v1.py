b705b4d1c265b6315ef7d8ae28c218b82fba316f
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-12 21:56:51 +0300

    Updated pantry item: menus

--- buridan_ui/pantry/menus/v1.py
+++ buridan_ui/pantry/menus/v1.py
@@ -3,7 +3,7 @@ import reflex as rx
 
 class Content(rx.State):
     links: list[dict[str, str]] = [
-        {"label": "Usage", "link": "#usage", "color": "hsl(160.1 84.1% 39.4%)"},
+        {"label": "Usage", "link": "#usage", "color": "gray"},
         {"label": "Position and placement", "link": "#position", "color": "gray"},
         {"label": "With other overlays", "link": "#overlays", "color": "gray"},
         {"label": "Manage focus", "link": "#focus", "color": "gray"},
@@ -19,9 +19,9 @@ class Content(rx.State):
     async def toggle_table_content(self, index: int, item: dict[str, str]):
         self.links = [
             (
-                {**data, "color": "gray"}
+                {**data, "color": rx.color("slate", 11)}
                 if data["label"] != item["label"]
-                else {**data, "color": "hsl(160.1 84.1% 39.4%)"}
+                else {**data, "color": rx.color("slate", 12)}
             )
             for data in self.links
         ]
@@ -41,29 +41,29 @@ def items(index: int, data: dict[str, str]):
             href=data["link"],
             text_decoration="none",
         ),
-        border_left="1px solid hsl(0, 0%, 20%)",
+        border_left=f"1px solid {rx.color('gray', 5)}",
         width="200px",
         height="30px",
         align="center",
         justify="start",
         padding_left="15px",
-        _hover={"bg": "hsl(0, 0%, 25%)"},
         border_radius="0px 5px 5px 0px",
     )
 
 
 def item_header():
     return rx.hstack(
-        rx.icon(tag="puzzle", size=12),
         rx.text("Menu Items", font_size="11px", color_scheme="gray", weight="medium"),
+        rx.icon(tag="puzzle", size=12),
         width="100%",
-        justify="start",
+        justify="between",
         align="center",
-        border_left="1px solid hsl(0, 0%, 20%)",
+        border_left=f"1px solid {rx.color('gray', 5)}",
         border_radius="0px 5px 5px 0px",
         padding_left="15px",
+        padding_right="15px",
         height="30px",
-        bg="hsl(0 0% 16%)",
+        bg=rx.color("gray", 3),
     )
 
 
@@ -75,8 +75,7 @@ def menus_v1():
                 width="10px",
                 height="10px",
                 border_radius="10px",
-                border="1px solid hsl(160.1 84.1% 39.4%)",
-                bg="hsl(0, 0%, 15%)",
+                bg=rx.color("blue"),
                 position="absolute",
                 left="-4.5px",
                 top=Content.position_y,
