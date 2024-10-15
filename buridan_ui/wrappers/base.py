736d57f5517d41ac8149311de034c20ec2836e6c
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-15 09:58:13 +0300

    Updated base wrapper

--- buridan_ui/wrappers/base.py
+++ buridan_ui/wrappers/base.py
@@ -1,3 +1,4 @@
+from ..templates.shared.drawbar import drawbar
 from ..templates.shared.navbar import navbar_type_v1
 from ..templates.shared.sidebar import sidebar, Sidebar
 from ..templates.shared.footer import footer, footer_v1
@@ -9,39 +10,31 @@ from functools import wraps
 
 import reflex as rx
 
-PADDING = [
-    "108px 12px 72px 12px",
-    "108px 12px 72px 12px",
-    "108px 12px 72px 12px",
-    "108px 48px 72px 48px",
-    "108px 64px 72px 64px",
-    "108px 128px 72px 128px",
-]
-
 GRANDPARENT = dict(
     spacing="0",
     width="100%",
     height="100vh",
-    overflow="hidden",
+    overflow="scroll",
     scrollbar_width="thin",
+    background=rx.color("gray", 2),
+    overscroll_behavior_y="contain",
 )
 
 PARENT = dict(
     spacing="0",
     width="100%",
+    height="100vh",
     overflow="auto",
     scrollbar_width="thin",
+    background=rx.color("gray", 2),
 )
 
 CONTENT = dict(
     z_index="5",
     width="100%",
     spacing="9",
-    padding="54px 0px",
-    overflow_y="auto",
-    min_height="100vh",
+    padding="54px 0px 0px 0px",
     position="relative",
-    overflow_x="hidden",
     transition="all 350ms ease",
     background=rx.color("gray", 3),
 )
@@ -49,14 +42,11 @@ CONTENT = dict(
 HEADER = dict(
     top="0",
     left="0",
-    right="0",
     spacing="2",
     width="100%",
     align="center",
-    padding_top="60px",
-    height="20vh",
+    min_height="25vh",
     justify="center",
-    position="sticky",
     background=rx.color("gray", 2),
     border_left=f"1px solid {rx.color('gray', 4)}",
 )
@@ -92,19 +82,19 @@ def capitalize_words(segment: str) -> str:
 def render_prev_and_next_ui(routes: list[dict[str, str]]):
     _prev, _next = routes
 
-    # Create buttons or spacers based on availability
     prev_button = (
         rx.hstack(
-            rx.icon(tag="arrow-left", size=15),
+            rx.icon(tag="arrow-left", size=15, color=rx.color("slate", 12)),
             rx.link(
                 rx.text(
                     _prev["name"],
                     weight="bold",
                     size="1",
                     on_click=Sidebar.delta_page(_prev),
+                    color=rx.color("slate", 12),
                 ),
                 href=_prev["path"],
-                color_scheme="gray",
+                underline="none",
             ),
             align="center",
             spacing="2",
@@ -120,11 +110,12 @@ def render_prev_and_next_ui(routes: list[dict[str, str]]):
                     weight="bold",
                     size="1",
                     on_click=Sidebar.delta_page(_next),
+                    color=rx.color("slate", 12),
                 ),
                 href=_next["path"],
-                color_scheme="gray",
+                underline="none",
             ),
-            rx.icon(tag="arrow-right", size=15),
+            rx.icon(tag="arrow-right", size=15, color=rx.color("slate", 12)),
             align="center",
             spacing="2",
         )
@@ -138,13 +129,13 @@ def render_prev_and_next_ui(routes: list[dict[str, str]]):
             next_button,
             justify="between",
             width="100%",
-            # background=rx.color("gray", 2),
         ),
-        position="absolute",
+        position="sticky",
         bottom="0",
         width="100%",
         padding="14px 24px",
         radius="none",
+        bg=rx.color("blue", 5),
     )
 
 
@@ -166,21 +157,26 @@ def base(url: str, page_name: str, **kwargs):
         def template(*args, **kwargs):
             contents = content(*args, **kwargs)
             return rx.vstack(
+                drawbar(),
                 navbar_type_v1(),
                 rx.hstack(
                     sidebar(),
                     rx.vstack(
                         rx.vstack(
-                            create_route_ui(url),
-                            title(page_name),
-                            **HEADER,
+                            rx.vstack(
+                                rx.vstack(
+                                    create_route_ui(url),
+                                    title(page_name),
+                                    **HEADER,
+                                ),
+                                *contents,
+                                rx.divider(height="4em", opacity="0"),
+                                create_page_prev_and_next_navigation(url),
+                                **CONTENT,
+                            ),
+                            width="100%",
+                            position="relative",
                         ),
-                        rx.vstack(
-                            *contents,
-                            create_page_prev_and_next_navigation(url),
-                            **CONTENT,
-                        ),
-                        # footer(),
                         footer_v1(),
                         width="100%",
                         spacing="0",
