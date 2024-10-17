79eacf76d28cddea218821ddfe18f8686192973d
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-17 19:56:22 +0300

    Updated chart routes

--- buridan_ui/routes/chart_routes.py
+++ buridan_ui/routes/chart_routes.py
@@ -1,5 +1,6 @@
 CHART_ROUTES = [
     {"name": "Bar Charts", "path": "/charts/bar-charts"},
+    {"name": "Area Charts", "path": "/charts/area-charts"},
 ]
 
 CHART_ROUTES.sort(key=lambda x: x["name"])
