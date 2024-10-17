8856bec8a1a0170e3da103c2d1e1c74e5587f7ca
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-17 19:56:53 +0300

    Updated app pages

--- buridan_ui/pages/__init__.py
+++ buridan_ui/pages/__init__.py
@@ -1,13 +1,10 @@
 from .index import index
 
+from .started_items.introduction import introduction
+from .started_items.installation import installation
+from .started_items.buridan import buridan
 
-# from .started_items.introduction import introduction
-# from .started_items.installation import installation
-# from .started_items.buridan import buridan
-#
-# from .interactive_tables.dashboard import dashboard
+from .interactive_tables.dashboard import dashboard
 
-# from .pantry_items.items import *
+from .pantry_items.items import *
 from .chart_items.items import *
-
-# from .pantry_items.items import subscribe
