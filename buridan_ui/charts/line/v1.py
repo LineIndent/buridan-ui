7c0b215b5547caed05094580ddc6f2d5d04ca178
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-18 19:54:47 +0300

    Added new chart item: line charts

---
+++ buridan_ui/charts/line/v1.py
@@ -0,0 +1,60 @@
+import reflex as rx
+from ...wrappers.item import Item
+
+data = [
+    {"month": "Jan", "desktop": 186},
+    {"month": "Feb", "desktop": 305},
+    {"month": "Mar", "desktop": 237},
+    {"month": "Apr", "desktop": 73},
+    {"month": "May", "desktop": 209},
+    {"month": "Jun", "desktop": 214},
+]
+
+
+def linechart_v1():
+    return rx.center(
+        rx.vstack(
+            rx.vstack(
+                rx.heading("Line Chart", size="5", weight="bold"),
+                rx.text("January - June 2024", size="1", color=rx.color("slate", 11)),
+                spacing="1",
+            ),
+            rx.divider(height="1rem", opacity="0"),
+            rx.recharts.line_chart(
+                rx.recharts.graphing_tooltip(
+                    label_style={"fontWeight": "700"}, item_style={"padding": "0px"}
+                ),
+                rx.recharts.cartesian_grid(
+                    horizontal=True,
+                    vertical=False,
+                    fill_opacity=0.5,
+                    stroke=rx.color("slate", 5),
+                ),
+                rx.recharts.line(
+                    data_key="desktop",
+                    stroke=Item.default_theme[1],
+                    type_="natural",
+                    dot=False,
+                ),
+                rx.recharts.x_axis(data_key="month", axis_line=False),
+                data=data,
+                width="100%",
+                height=250,
+                margin={"left": 20},
+            ),
+            rx.vstack(
+                rx.heading("Trending up by 5.2% this month", size="3", weight="bold"),
+                rx.text(
+                    "Showing total visitors for the last 6 months",
+                    size="1",
+                    color=rx.color("slate", 11),
+                ),
+                spacing="1",
+            ),
+            width="100%",
+            margin_right="10px",
+            max_width=["80em", "70em", "60em", "50em", "40em", "30em"],
+        ),
+        width="100%",
+        padding="12px",
+    )
