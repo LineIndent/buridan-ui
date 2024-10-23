238f9cf033a99d7d14d6e864ace2139cf9b8e214
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-23 20:41:37 +0300

    Updated gettig started routes

--- buridan_ui/routes/started_routes.py
+++ buridan_ui/routes/started_routes.py
@@ -2,4 +2,5 @@ GETTING_STARTED_ROUTES = [
     {"name": "Introduction", "path": "/getting-started/introduction"},
     {"name": "Installation", "path": "/getting-started/installation"},
     {"name": "Who is Buridan?", "path": "/getting-started/who-is-buridan"},
+    {"name": "Changelog", "path": "/pro/changelog"},
 ]
