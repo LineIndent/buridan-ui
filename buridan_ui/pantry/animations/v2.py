0ae29d7e396cb049a1d81b7d8421f0638a330b91
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-28 16:31:23 +0200

    Updated code structure for animation v2.py

--- buridan_ui/pantry/animations/v2.py
+++ buridan_ui/pantry/animations/v2.py
@@ -1,14 +1,43 @@
-import asyncio
+from dataclasses import dataclass, field
 import reflex as rx
+import asyncio
 
-right: dict[str, any] = {
-    "position": "relative",
-    "animation": "rightSlide 0.6s",
-    "@keyframes rightSlide": {
-        "from": {"right": "-300px", "opacity": "0"},
-        "to": {"right": "0px", "opacity": "1"},
-    },
-}
+
+@dataclass
+class Style:
+    base: dict[str, str] = field(
+        default_factory=lambda: {
+            "width": "100%",
+            "height": "20em",
+            "align": "center",
+            "justify": "center",
+            "position": "relative",
+        }
+    )
+
+    button: dict[str, str] = field(
+        default_factory=lambda: {
+            "top": "-24px",
+            "right": "-12px",
+            "cursor": "pointer",
+            "position": "absolute",
+            "border_radius": "0px 0px 0px 8px",
+        }
+    )
+
+    right: dict[str, str] = field(
+        default_factory=lambda: {
+            "position": "relative",
+            "animation": "rightSlide 0.6s",
+            "@keyframes rightSlide": {
+                "from": {"right": "-300px", "opacity": "0"},
+                "to": {"right": "0px", "opacity": "1"},
+            },
+        }
+    )
+
+
+Style: Style = Style()
 
 
 class Animation(rx.State):
@@ -20,7 +49,7 @@ class Animation(rx.State):
             return
 
         self.is_disabled = True
-        self.animate = right
+        self.animate = Style.right
         yield  # Allow the animation to apply
 
         await asyncio.sleep(1)  # Wait for the animation to finish
@@ -33,17 +62,9 @@ def animation_v2():
         rx.heading("buridan/ui", size="5", font_weight="900", style=Animation.animate),
         rx.button(
             "Run",
-            top="-24px",
-            right="-12px",
-            cursor="pointer",
-            position="absolute",
             loading=Animation.is_disabled,
-            border_radius="0px 0px 0px 8px",
             on_click=Animation.run_animation,
+            **Style.button,
         ),
-        width="100%",
-        height="20em",
-        align="center",
-        justify="center",
-        position="relative",
+        **Style.base,
     )
