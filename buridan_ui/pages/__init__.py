ede6c5e3f9e74cee333fcfff84d8a08561954128
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-10-30 17:38:15 +0200

    Updated pages

--- buridan_ui/pages/__init__.py
+++ buridan_ui/pages/__init__.py
@@ -1,12 +1,12 @@
 from .index import index
 from .changelog import changelog
 
-
 from .started_items.introduction import introduction
 from .started_items.installation import installation
 from .started_items.buridan import buridan
 
-from .interactive_tables.dashboard import dashboard
+from .interactive.dashboard import dashboard
+from .interactive.rag.main import rag_ai_app
 
 from .pantry_items.items import *
 from .chart_items.items import *
