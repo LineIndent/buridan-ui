659ee49e1866b1d3128d67a4139173e61d84d271
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-01 20:04:40 +0200

    New routes for resources_routes.py

---
+++ buridan_ui/routes/resources_routes.py
@@ -0,0 +1,6 @@
+RESOURCES = [
+    {"name": "Reflex Framework", "path": "https://reflex.dev/"},
+    {"name": "Source Code", "path": "https://github.com/LineIndent/buridan-ui"},
+    {"name": "GitHub", "path": "https://github.com/LineIndent"},
+    {"name": "@LineIndent", "path": "https://www.youtube.com/@lineindent"},
+]
