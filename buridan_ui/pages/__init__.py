e20adac7c84a90b7c2777315f75cdf89d81148f1
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-02 19:06:36 +0200

    Update side __init__ pages

--- buridan_ui/pages/__init__.py
+++ buridan_ui/pages/__init__.py
@@ -1,10 +1,9 @@
 from .landing.hero import landing_page
 
 from .changelog import changelog
-
-from .started_items.introduction import introduction
-from .started_items.installation import installation
 from .started_items.buridan import buridan
+from .started_items.installation import installation
+from .started_items.introduction import introduction
 
 from .interactive.dashboard import dashboard
 from .interactive.rag.main import rag_ai_app
