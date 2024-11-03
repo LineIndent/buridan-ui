5405fbd5ee9b2ebcfdada9cbe623a8f8867c9ead
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-03 16:14:37 +0200

    Removed unused files and folders

--- buridan_ui/pages/landing/navigation/navigation.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-
-from .style import LandingPageNavigationStyle
-from ....templates.shared.navbar import right_items, left_items
-
-
-def landing_page_navigation():
-    return rx.hstack(left_items(), right_items(), **LandingPageNavigationStyle.style)
