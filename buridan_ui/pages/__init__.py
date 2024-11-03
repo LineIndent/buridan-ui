f00557d230f62dbd8889514d7823a7b585ccb214
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-03 16:14:08 +0200

    Directory re-structuring for site

--- buridan_ui/pages/__init__.py
+++ buridan_ui/pages/__init__.py
@@ -1,9 +1,9 @@
 from .landing.hero import landing_page
 
-from .changelog import changelog
 from .started_items.buridan import buridan
 from .started_items.installation import installation
 from .started_items.introduction import introduction
+from .changelog.changelog import changelog
 
 from .interactive.dashboard import dashboard
 from .interactive.rag.main import rag_ai_app
