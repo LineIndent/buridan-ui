166ae5255aeaf80865f1420af0f59636a2c3b762
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-07 17:05:52 +0200

    Updated routes class

--- buridan_ui/routes/routes.py
+++ buridan_ui/routes/routes.py
@@ -4,12 +4,30 @@ from typing import List, Dict
 
 @dataclass
 class Routes:
+    landing: dict[str, str] = field(default_factory=lambda: {"path": "/"})
+
     started: List[Dict[str, str]] = field(
         default_factory=lambda: [
-            {"name": "Introduction", "path": "/getting-started/introduction"},
-            {"name": "Installation", "path": "/getting-started/installation"},
-            {"name": "Who is Buridan?", "path": "/getting-started/who-is-buridan"},
-            {"name": "Changelog", "path": "/getting-started/changelog"},
+            {
+                "name": "Introduction",
+                "path": "/getting-started/introduction",
+                "dir": "introduction",
+            },
+            {
+                "name": "Installation",
+                "path": "/getting-started/installation",
+                "dir": "installation",
+            },
+            {
+                "name": "Who is Buridan?",
+                "path": "/getting-started/who-is-buridan",
+                "dir": "buridan",
+            },
+            {
+                "name": "Changelog",
+                "path": "/getting-started/changelog",
+                "dir": "changelog",
+            },
         ]
     )
 
@@ -26,38 +44,105 @@ class Routes:
 
     pantries: List[Dict[str, str]] = field(
         default_factory=lambda: [
-            {"name": "Logins", "path": "/pantry/logins"},
-            {"name": "Standard Forms", "path": "/pantry/standard-forms"},
-            {"name": "Standard Tables", "path": "/pantry/standard-tables"},
-            {"name": "Pricing Sections", "path": "/pantry/pricing-sections"},
-            {"name": "Popups", "path": "/pantry/popups"},
-            {"name": "Payments & Billing", "path": "/pantry/payments-and-billing"},
-            {"name": "Table Pagination", "path": "/pantry/table-pagination"},
+            {"name": "Logins", "path": "/pantry/logins", "dir": "logins"},
+            {
+                "name": "Standard Forms",
+                "path": "/pantry/standard-forms",
+                "dir": "forms",
+            },
+            {
+                "name": "Standard Tables",
+                "path": "/pantry/standard-tables",
+                "dir": "tables",
+            },
+            {
+                "name": "Pricing Sections",
+                "path": "/pantry/pricing-sections",
+                "dir": "pricing",
+            },
+            {
+                "name": "Popups",
+                "path": "/pantry/popups",
+                "dir": "popups",
+            },
+            {
+                "name": "Payments & Billing",
+                "path": "/pantry/payments-and-billing",
+                "dir": "payments",
+            },
+            {
+                "name": "Table Pagination",
+                "path": "/pantry/table-pagination",
+                "dir": "tables",
+            },
             {
                 "name": "Onboarding & Progress",
                 "path": "/pantry/onboarding-and-progress",
+                "dir": "onboardings",
             },
-            {"name": "Menus", "path": "/pantry/menus"},
-            {"name": "Backgrounds", "path": "/pantry/backgrounds"},
-            {"name": "Featured", "path": "/pantry/featured"},
-            {"name": "Descriptive Lists", "path": "/pantry/descriptive-lists"},
-            {"name": "Timeline", "path": "/pantry/timeline"},
-            {"name": "Animations", "path": "/pantry/animations"},
-            {"name": "Prompt Boxes", "path": "/pantry/prompt-boxes"},
-            {"name": "Cards", "path": "/pantry/cards"},
-            {"name": "Subscribe", "path": "/pantry/subscribe"},
+            {
+                "name": "Menus",
+                "path": "/pantry/menus",
+                "dir": "menus",
+            },
+            {
+                "name": "Backgrounds",
+                "path": "/pantry/backgrounds",
+                "dir": "backgrounds",
+            },
+            {
+                "name": "Featured",
+                "path": "/pantry/featured",
+                "dir": "featured",
+            },
+            {
+                "name": "Descriptive Lists",
+                "path": "/pantry/descriptive-lists",
+                "dir": "lists",
+            },
+            {
+                "name": "Timeline",
+                "path": "/pantry/timeline",
+                "dir": "timeline",
+            },
+            {
+                "name": "Animations",
+                "path": "/pantry/animations",
+                "dir": "animations",
+            },
+            {
+                "name": "Prompt Boxes",
+                "path": "/pantry/prompt-boxes",
+                "dir": "prompts",
+            },
+            {
+                "name": "Cards",
+                "path": "/pantry/cards",
+                "dir": "cards",
+            },
+            {
+                "name": "Subscribe",
+                "path": "/pantry/subscribe",
+                "dir": "subscribe",
+            },
             {
                 "name": "Frequently Asked Questions",
                 "path": "/pantry/frequently-asked-questions",
+                "dir": "faq",
             },
-            {"name": "Footers", "path": "/pantry/footers"},
+            {
+                "name": "Footers",
+                "path": "/pantry/footers",
+                "dir": "footers",
+            },
         ]
     )
+
     charts: List[Dict[str, str]] = field(
         default_factory=lambda: [
-            {"name": "Bar Charts", "path": "/charts/bar-charts"},
-            {"name": "Area Charts", "path": "/charts/area-charts"},
-            {"name": "Line Charts", "path": "/charts/line-charts"},
+            {"name": "Bar Charts", "path": "/charts/bar-charts", "dir": "bar"},
+            {"name": "Area Charts", "path": "/charts/area-charts", "dir": "area"},
+            {"name": "Line Charts", "path": "/charts/line-charts", "dir": "line"},
         ]
     )
 
