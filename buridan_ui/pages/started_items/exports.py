8d85b21b63828ad871e187597ace26c42b0b979d
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-07 17:10:58 +0200

    New config var for `getting started` files

---
+++ buridan_ui/pages/started_items/exports.py
@@ -0,0 +1,12 @@
+from .buridan import buridan
+from .installation import installation
+from .introduction import introduction
+from ..changelog.changelog import changelog
+
+
+getting_started_config = {
+    "buridan": [buridan],
+    "installation": [installation],
+    "introduction": [introduction],
+    "changelog": [changelog],
+}
