e6803daeac98fde45b53695a6a3f1b6332dbd63c
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-07 17:11:24 +0200

    Updated __init__.py for pages

--- buridan_ui/pages/__init__.py
+++ buridan_ui/pages/__init__.py
@@ -1,14 +1,4 @@
-from .landing.hero import landing_page
-
-from .blueprints.blueprint import blueprint
-
-from .started_items.buridan import buridan
-from .started_items.installation import installation
-from .started_items.introduction import introduction
-from .changelog.changelog import changelog
+# from .blueprints.blueprint import blueprint
 
 from .interactive.dashboard import dashboard
 from .interactive.rag.main import rag_ai_app
-
-from .pantry_items.items import *
-from .chart_items.items import *
