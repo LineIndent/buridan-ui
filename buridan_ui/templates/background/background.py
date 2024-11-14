b8733d8a086cf13c122d4bd4c3aee25cb8e4dff9
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-14 15:14:43 +0200

    Moved landing page background to templates

--- buridan_ui/templates/background/background.py
+++ buridan_ui/templates/background/background.py
@@ -22,9 +22,29 @@ class BackgroundStyle:
         }
     )
 
+    landing: dict[str, str] = field(
+        default_factory=lambda: {
+            "background_size": "34px 34px",
+            "background_image": f"radial-gradient(circle, {rx.color('slate', 12)} 1px, transparent 1px)",
+            "mask": (
+                "radial-gradient(45% 45% at 50% 50%, hsl(0, 0%, 0%, 0.60), hsl(0, 0%, 0%, 0)), "
+                "radial-gradient(60% 70% at 50% 50%, hsl(0, 0%, 0%, 0.35), hsl(0, 0%, 0%, 0))"
+            ),
+            "width": "100%",
+            "height": "100vh",
+            "position": "absolute",
+            "top": "0",
+            "left": "0",
+        }
+    )
+
 
 BackgroundStyle: BackgroundStyle = BackgroundStyle()
 
 
 def grid_background():
     return rx.box(**BackgroundStyle.grid)
+
+
+def landing_page_grid_background():
+    return rx.box(**BackgroundStyle.landing)
