0119f893a59cbdfff89c566b53ddde6d99ddd8af
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-17 17:29:57 +0200

    Updated landing page background

--- buridan_ui/templates/background/background.py
+++ buridan_ui/templates/background/background.py
@@ -24,14 +24,14 @@ class BackgroundStyle:
 
     landing: dict[str, str] = field(
         default_factory=lambda: {
-            "background_size": "34px 34px",
+            "background_size": "30px 30px",
             "background_image": f"radial-gradient(circle, {rx.color('slate', 12)} 1px, transparent 1px)",
             "mask": (
                 "radial-gradient(45% 45% at 50% 50%, hsl(0, 0%, 0%, 0.60), hsl(0, 0%, 0%, 0)), "
                 "radial-gradient(60% 70% at 50% 50%, hsl(0, 0%, 0%, 0.35), hsl(0, 0%, 0%, 0))"
             ),
             "width": "100%",
-            "height": "100vh",
+            "height": "110vh",
             "position": "absolute",
             "top": "0",
             "left": "0",
