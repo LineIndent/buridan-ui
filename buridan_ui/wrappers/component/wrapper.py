094d84c0802632e9eed7f4fb8910b6977735976a
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-11-20 14:38:38 +0200

    Created new wrapper for blueprint items

--- buridan_ui/wrappers/component/wrapper.py
+++ buridan_ui/wrappers/component/wrapper.py
@@ -8,7 +8,10 @@ from reflex.components.datadisplay.code import Theme
 from .state import ComponentWrapperState
 from .style import ComponentWrapperStyle, InnerCode
 
-from .utils.tabs import component_wrapper_tab_menu
+from .utils.tabs import (
+    component_wrapper_tab_menu,
+    component_wrapper_tab_menu_blueprints,
+)
 from .utils.responsive import component_wrapper_responsive_menu
 from .utils.source import component_wrapper_source_code
 from .utils.scheme import component_wrapper_color_scheme
@@ -32,18 +35,45 @@ def component_wrapper_menu_bar(has_theme: bool, component_id: int, path: str):
 
 
 def component_wrapper_preview_content(component: rx.Component, component_id: int):
-    return (
-        rx.tabs.content(
-            rx.vstack(
-                component,
-                width=[
-                    ("100%" if i <= 3 else ComponentWrapperState.uuid[component_id])
-                    for i in range(6)
-                ],
-                **ComponentWrapperStyle.preview,
-            ),
-            value="1",
+    return rx.tabs.content(
+        rx.vstack(
+            component,
+            width=[
+                ("100%" if i <= 3 else ComponentWrapperState.uuid[component_id])
+                for i in range(6)
+            ],
+            **ComponentWrapperStyle.preview,
         ),
+        value="1",
+    )
+
+
+def component_wrapper_code_base(
+    component_code: str,
+    value: str = "2",
+) -> rx.tabs.content:
+    return rx.tabs.content(
+        rx.hstack(
+            rx.code_block(
+                component_code,
+                theme=Theme.darcula,
+                **ComponentWrapperStyle.code,
+            ),
+            rx.button(
+                rx.cond(
+                    ComponentWrapperState.default_icon,
+                    rx.icon(tag="clipboard-list", size=14),
+                    rx.icon(tag="check", size=14, color=rx.color("grass")),
+                ),
+                on_click=[
+                    ComponentWrapperState.toggle_icon,
+                    rx.set_clipboard(component_code),
+                ],
+                **ComponentWrapperStyle.copy_button,
+            ),
+            **InnerCode,
+        ),
+        value=value,
     )
 
 
@@ -83,7 +113,7 @@ def component_wrapper(path: str, has_theme: bool = False):
                 rx.tabs.root(
                     component_wrapper_menu_bar(has_theme, component_id, path),
                     component_wrapper_preview_content(component, component_id),
-                    component_wrapper_code_content(component_code),
+                    component_wrapper_code_base(component_code),
                     **ComponentWrapperStyle.root,
                 ),
                 width="100%",
@@ -94,17 +124,32 @@ def component_wrapper(path: str, has_theme: bool = False):
     return decorator
 
 
+blueprintFrontEndContent: Callable[[str], rx.tabs.content] = (
+    lambda code: component_wrapper_code_base(code, value="2")
+)
+
+blueprintStyleContent: Callable[[str], rx.tabs.content] = (
+    lambda code: component_wrapper_code_base(code, value="3")
+)
+
+blueprintStateContent: Callable[[str], rx.tabs.content] = (
+    lambda code: component_wrapper_code_base(code, value="4")
+)
+
+
 def blueprint_wrapper():
-    def decorator(func: Callable[[], rx.Component]):
+    def decorator(func: Callable[[], list[rx.Component | str | int]]):
         @wraps(func)
         def wrapper():
-            component = func()
+            component, preview, style, state, _ = func()
 
             return rx.vstack(
                 rx.tabs.root(
-                    component_wrapper_tab_menu(),
+                    component_wrapper_tab_menu_blueprints(),
                     component_wrapper_preview_content(component, 0),
-                    component_wrapper_code_content(""),
+                    component_wrapper_code_base(preview, "2"),
+                    component_wrapper_code_base(style, "3"),
+                    component_wrapper_code_base(state, "4"),
                     **ComponentWrapperStyle.root,
                 ),
                 width="100%",
