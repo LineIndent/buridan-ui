c145d90d5a119043400343c2ec659c81eda80109
pourhakimi <84860195+pourhakimi@users.noreply.github.com>
2024-12-02 12:28:26 +0200

    Updated Area Charts with new UI elements and logic

--- buridan_ui/charts/area/v4.py
+++ buridan_ui/charts/area/v4.py
@@ -1,30 +1,30 @@
 import reflex as rx
+
+from ..style import tooltip_styles, info
 from ...wrappers.state import ComponentWrapperState
 
 
-data = [
-    {"month": "Jan", "desktop": 186, "mobile": 80},
-    {"month": "Feb", "desktop": 305, "mobile": 200},
-    {"month": "Mar", "desktop": 237, "mobile": 120},
-    {"month": "Apr", "desktop": 73, "mobile": 190},
-    {"month": "May", "desktop": 209, "mobile": 130},
-    {"month": "Jun", "desktop": 214, "mobile": 140},
-]
-
-
 def areachart_v4():
+
+    data = [
+        {"month": "Jan", "desktop": 186, "mobile": 80},
+        {"month": "Feb", "desktop": 305, "mobile": 200},
+        {"month": "Mar", "desktop": 237, "mobile": 120},
+        {"month": "Apr", "desktop": 73, "mobile": 190},
+        {"month": "May", "desktop": 209, "mobile": 130},
+        {"month": "Jun", "desktop": 214, "mobile": 140},
+    ]
+
     return rx.center(
         rx.vstack(
-            rx.vstack(
-                rx.heading("Area Chart - Stacked", size="4", weight="bold"),
-                rx.text("January - June 2024", size="1", color=rx.color("slate", 11)),
-                spacing="1",
+            info(
+                "Area Chart - Stacked",
+                "3",
+                "Showing total visitors for the last 6 months",
+                "start",
             ),
-            rx.divider(height="1rem", opacity="0"),
             rx.recharts.area_chart(
-                rx.recharts.graphing_tooltip(
-                    label_style={"fontWeight": "700"}, item_style={"padding": "0px"}
-                ),
+                rx.recharts.graphing_tooltip(**vars(tooltip_styles)),
                 rx.recharts.cartesian_grid(
                     horizontal=True,
                     vertical=False,
@@ -40,24 +40,22 @@ def areachart_v4():
                     )
                     for index, name in enumerate(["desktop", "mobile"])
                 ],
-                rx.recharts.x_axis(data_key="month", axis_line=False),
+                rx.recharts.x_axis(
+                    data_key="month",
+                    axis_line=False,
+                    tick_size=10,
+                    tick_line=False,
+                    custom_attrs={"fontSize": "12px"},
+                ),
                 data=data,
                 width="100%",
                 height=250,
                 margin={"left": 20},
             ),
-            rx.vstack(
-                rx.heading("Trending up by 5.2% this month", size="2", weight="bold"),
-                rx.text(
-                    "Showing total visitors for the last 6 months",
-                    size="1",
-                    color=rx.color("slate", 11),
-                ),
-                spacing="1",
-            ),
+            info("Trending up by 5.2% this month", "2", "January - June 2024", "start"),
             width="100%",
-            margin_right="10px",
+            margin_right="20px",
         ),
         width="100%",
-        padding="12px",
+        padding="0.5em",
     )
